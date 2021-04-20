df1a82bf2ec4253227b0c7b18137241ac19686f0
jonasmedcapital <jonastrinda@gmail.com>
2021-04-20 13:31:06 -0300

    nao sei

--- app/javascript/controllers/pages/modal_controller.js
+++ app/javascript/controllers/pages/modal_controller.js
@@ -1,14 +1,25 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["main", "image", "text", "dates", "startedAtInput", "selecthour", "", "", ""]
+  static targets = ["main", "image", "text", "dates", "startedAtInput", "selectHour", "secondStep", "thirdStep", "cardTitle", "cardBody", "buttonHour"]
 
   connect() {
     this.availableDates = { dates: [
       { id: 1, day: 22, month: "Abr", year: 2021, hour: "08:00", current_user_id: 1 },
       { id: 2, day: 22, month: "Abr", year: 2021, hour: "09:00", current_user_id: 1 },
-      { id: 3, day: 22, month: "Abr", year: 2021, hour: "09:00", current_user_id: 2 },
-      { id: 4, day: 23, month: "Abr", year: 2021, hour: "08:00", current_user_id: 1 },
+      { id: 3, day: 22, month: "Abr", year: 2021, hour: "10:00", current_user_id: 2 },
+      { id: 16, day: 22, month: "Abr", year: 2021, hour: "11:00", current_user_id: 2 },
+      { id: 17, day: 22, month: "Abr", year: 2021, hour: "12:00", current_user_id: 2 },
+      { id: 18, day: 22, month: "Abr", year: 2021, hour: "13:00", current_user_id: 2 },
+      { id: 19, day: 22, month: "Abr", year: 2021, hour: "14:00", current_user_id: 2 },
+      { id: 20, day: 22, month: "Abr", year: 2021, hour: "15:00", current_user_id: 2 },
+      { id: 21, day: 22, month: "Abr", year: 2021, hour: "16:00", current_user_id: 2 },
+      { id: 22, day: 22, month: "Abr", year: 2021, hour: "17:00", current_user_id: 2 },
+      { id: 23, day: 22, month: "Abr", year: 2021, hour: "19:00", current_user_id: 2 },
+      { id: 24, day: 22, month: "Abr", year: 2021, hour: "20:00", current_user_id: 2 },
+      { id: 25, day: 22, month: "Abr", year: 2021, hour: "21:00", current_user_id: 2 },
+      { id: 26, day: 22, month: "Abr", year: 2021, hour: "22:00", current_user_id: 2 },
+      { id: 27, day: 23, month: "Abr", year: 2021, hour: "08:00", current_user_id: 1 },
       { id: 5, day: 23, month: "Abr", year: 2021, hour: "09:00", current_user_id: 1 },
       { id: 6, day: 23, month: "Abr", year: 2021, hour: "10:00", current_user_id: 2 },
       { id: 7, day: 24, month: "Abr", year: 2021, hour: "08:00", current_user_id: 1 },
@@ -21,8 +32,9 @@ export default class extends Controller {
       { id: 14, day: 26, month: "Abr", year: 2021, hour: "09:00", current_user_id: 1 },
       { id: 15, day: 26, month: "Abr", year: 2021, hour: "11:00", current_user_id: 2 },
     ]}
+
+    this.clientName = "Caio"
     this.controllerName = "pages--modal"
-    console.log("this is home stimulus")
     this.doMainHtml()
   }
 
@@ -52,7 +64,7 @@ export default class extends Controller {
 
   doTexHtml() {
 
-    var html = `<h2 class="colortextmodal"><b>Obrigado Caio, agende aqui sua consultoria gratuita!</b></h2>`
+    var html = `<h2 class="colortextmodal"><b>Obrigado ${this.clientName}, agende aqui sua consultoria gratuita!</b></h2>`
 
     this.textTarget.innerHTML = html
   }
@@ -64,32 +76,27 @@ export default class extends Controller {
                       <h3 class="texthrspace modalpassos "> <span class=""><i class="chip-icon">1</i> <b> Selecionar uma data</b></h3>
                     </div>
 
-                    <div class="d-flex divmodalpassos">
-                      <h3 class="texthrspace modalpassos "> <span class=""><i class="chip-icon">2</i> <b> Selecionar um horário</b></h3>
+                    <div data-target=${this.controllerName}.secondStep class="d-flex divmodalpassos">
                     </div>
 
-                    <div class="d-flex divmodalpassos">
-                      <h3 class="texthrspace modalpassos"> <span class=""><i class="chip-icon">3</i> <b> Confirmar Agendamento</b></h3>
+                    <div data-target=${this.controllerName}.thirdStep class="d-flex divmodalpassos">
                     </div>
                   
                   </div>
                 </div>
                 
                 <div class="col-6 d-flex flex-column">
-                  <div data-target="${this.controllerName}.selecthour" class="spacebuttons card">
+                  <div data-target="${this.controllerName}.selectHour" class="spacebuttons card">
 
-                      <div class="row d-flex justify-content-center">
+                      <div data-target=${this.controllerName}.cardTitle class="row d-flex justify-content-center">
                         <h3 class="texthrspace colorpassos"><b>Escolha a melhor data para sua consultoria.</b></h3>
                       </div>
-                     <div class="row d-flex justify-content-center mb-5">
-
-
-                      <button data-target="${this.controllerName}.startedAtInput" data-action="change->${this.controllerName}#changeDate" class="buttonfigma">Selecione uma data</button>
-
+                    <div data-target=${this.controllerName}.cardBody class="row d-flex justify-content-center scrollbar">
+                      <button data-target="${this.controllerName}.startedAtInput" data-action="change->${this.controllerName}#changeDate" class="button-date"><b></b>Selecione uma data</b></button>
                     </div>
                   </div>
 
-                  <div class=" spacebuttons col-sm-12 d-flex justify-content-center my-2" id="scheduleViaWhatsApp" style="">Não encontrou um horário ideal? <br><span><a class="a-white" target="blank" href="https://api.whatsapp.com/send?phone=5531995351912&amp;text=Olá, quero agendar uma consultoria. "><span class="ml-1" style="color:#26C485;"> Clique aqui </span> e fale diretamente com um consultor.</a></span></div>
+                  <div class=" col-sm-12 d-flex justify-content-center my-2" id="scheduleViaWhatsApp" style="">Não encontrou um horário ideal? <br><span><a class="a-white" target="blank" href="https://api.whatsapp.com/send?phone=5531995351912&amp;text=Olá, quero agendar uma consultoria. "><span class="ml-1" style="color:#26C485;"> Clique aqui </span> e fale diretamente com um consultor.</a></span></div>
 
                 </div>`
 
@@ -178,35 +185,71 @@ export default class extends Controller {
     this.avaiableHours = { hours: [], idHours: [] }
     
     this.availableDates.dates.forEach(element => {
-      
       if (day === element.day && month === element.month && year === element.year) {
-        console.log("achei")
         if (!this.avaiableHours.hours.includes(element.hour)) {
           this.avaiableHours.hours.push(element.hour)
           this.avaiableHours.idHours.push({id: element.id, hour: element.hour})
+          this.findedHour = true
         }
-      } else {
-        console.log("Não existe horas para essa data")
       }
     });
 
-    this.doHourHtml()
+    if(this.findedHour){
+      this.doHourHtml()
+    } else {
+      this.getControllerByIdentifier("app--helpers--snackbar").doSnackbar("danger", "Não existe horários disponíveis para essa data", 4000)
+      // this.doDateHtml()
+    }
   }
 
   doHourHtml(){
-    this.selecthourTarget.innerHTML = ` <div class="spacebuttons card">
+    if(this.startedAtInputTarget.value){
+      this.secondStepTarget.innerHTML = `<h3 class="texthrspace modalpassos "> <span class=""><i class="chip-icon">2</i> <b> Selecionar um horário</b></h3>`
+      this.cardTitleTarget.innerHTML = `<div class="row">
+                                          <div class="col-9">
+                                            <h3 class="texthrspace marginl colorpassos"><b>Selecione um horário</b></h3>
+                                          </div>
+                                          <div class="col-3 texthrspace margintneg">
+                                            <button data-action="click->${this.controllerName}#reloadSelectData" class="button-change-date"><b></b>Trocar data</b></button>
+                                          </div>
                                         </div>`
+      
+      var bodyHtml = ``
+
+      this.avaiableHours.idHours.forEach(element => {
+        bodyHtml += `<button id=${element.id} data-action="click->${this.controllerName}#selectHour" data-target="${this.controllerName}.buttonHour-${element.id}" class="button-hour text-center">${element.hour}</button>`
+      });
+      // this.cardBodyTarget.classList.add("flex-column")
+      this.cardBodyTarget.innerHTML = bodyHtml
+
+
+    } else {
+      this.getControllerByIdentifier("app--helpers--snackbar").doSnackbar("danger", "Selecione uma data", 4000)
+    }
   }
-  
-  // saveTest(){
-  //   var weeklyDate = new Date(this.getControllerByIdentifier("app--helpers--date").transformFullDate(this.startedAtInputTarget.value))
-  //   console.log(weeklyDate)
-  // }
+
+  reloadSelectData(){
+    this.findedHour = false
+    this.doDateHtml()
+  }
+
+  selectHour(ev){
+    this.thirdStepTarget.innerHTML = `<h3 class="texthrspace modalpassos"> <span class=""><i class="chip-icon">3</i> <b> Confirmar Agendamento</b></h3>`
+    this.cardTitleTarget.innerHTML = `<h3 class="texthrspace colorpassos"><b>Confirmar agendamento</b></h3>`
+    this.cardBodyTarget.innerHTML = `<button data-target="${this.controllerName}.startedAtInput" data-action="change->${this.controllerName}#changeDate" class="button-date">Confirmar</button>`
+    this.send_data = { current_user: { current_user_id: this.application.current_user_id}, hour: { hour_id: ev.target.id} }
+
+    // chamar fetch para o backend
+  }
 
   getControllerByIdentifier(identifier) {
     return this.application.controllers.find(controller => {
       return controller.context.identifier === identifier;
     });
+  }
+
+  nameTarget(target) {
+    return this.targets.find(target)
   }
 
 }
